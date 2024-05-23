fdaf85dfa35a0bd755b19ec8630f733fdfa214da
AntikKrasn <146510237+AntikKrasn@users.noreply.github.com>
2024-05-23 21:58:31 +0500

    Update load_diary.php

--- Diary/load_diary.php
+++ Diary/load_diary.php
@@ -4,40 +4,31 @@ session_start();
 require_once('db.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Проверяем, что данные были отправлены методом POST
     $note = htmlspecialchars($_POST['note']); // Получаем текст из поля textarea и очищаем его от специальных символов
-
-    // Получаем логин из сессии
     $login = $_SESSION['login'];
 
-    // Проверяем, существует ли уже запись для данного пользователя
     $sql_check = "SELECT * FROM diary WHERE login = ?";
     $stmt_check = $conn->prepare($sql_check);
     $stmt_check->bind_param("s", $login);
     $stmt_check->execute();
     $result_check = $stmt_check->get_result();
     
-
-    // Если запись уже существует, обновляем ее
     if ($result_check->num_rows > 0) {
         if (!empty($note)) {
-            // Если поле не пустое, обновляем запись
             $sql = "UPDATE diary SET diary_text = ? WHERE login = ?";
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("ss", $note, $login);
         } else {
-            // Если поле пустое, удаляем запись
             $sql = "DELETE FROM diary WHERE login = ?";
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("s", $login);
         }
-    } else { // Иначе вставляем новую запись
+    } else { 
         $sql = "INSERT INTO diary (login, diary_text) VALUES (?, ?)";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("ss", $login, $note);
     }
 
-    // Выполняем запрос
     if ($stmt->execute()) {
         echo "Данные успешно " . ($result_check->num_rows > 0 ? "обновлены" : "вставлены") . "!";
     } else {
@@ -46,7 +37,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $stmt->close();
 } else {
-    // Если запрос не был выполнен методом POST, возвращаем ошибку
     echo "Ошибка: Неверный метод запроса.";
 }
 ?>
