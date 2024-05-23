8bf3f0ec8a9a13d5972b295c7416899ad37445a3
AntikKrasn <146510237+AntikKrasn@users.noreply.github.com>
2024-05-23 22:10:21 +0500

    Update save_task.php

--- Task/save_task.php
+++ Task/save_task.php
@@ -1,39 +1,28 @@
 ﻿<?php
 require_once('db.php');
 error_reporting(0);
-session_start(); // начинаем сессию
-ob_start(); // Начинаем буферизацию вывода
+session_start(); 
+ob_start(); 
 
-// Проверяем, была ли отправлена задача через AJAX
 if(isset($_POST['task']) && !empty($_POST['task'])) {
-    // Проверяем, авторизован ли пользователь
     if(isset($_SESSION['login'])) {
-        // Получаем логин пользователя из сессии
         $login = $_SESSION['login'];
-
-        // Получаем задачу из POST-запроса
         $task = $_POST['task'];
-
-        // Подготовленный запрос для добавления задачи в таблицу `task`
         $sql_insert_task = "INSERT INTO `task` (task, login) VALUES (?, ?)";
         $stmt_insert_task = $conn->prepare($sql_insert_task);
         $stmt_insert_task->bind_param("ss", $task, $login);
 
-        // Выполнение подготовленного запроса
         if ($stmt_insert_task->execute()) {
             echo true;
         } else {
             echo "Ошибка при добавлении задачи: " . $conn->error;
         }
 
-        // Закрытие подготовленного запроса
         $stmt_insert_task->close();
     } else {
-        // Если пользователь не авторизован, выводим сообщение об ошибке
         echo "Ошибка: Пользователь не авторизован";
     }
 } else {
-    // Если задача не была отправлена или пустая, выводим сообщение об ошибке
     echo "Ошибка: Задача не была отправлена";
 }
 ?>
