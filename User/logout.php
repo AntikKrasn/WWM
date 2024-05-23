6109934c26d2ba9e4356f812a9a0c06c7e6278ed
AntikKrasn <146510237+AntikKrasn@users.noreply.github.com>
2024-05-23 22:02:12 +0500

    Update logout.php

--- User/logout.php
+++ User/logout.php
@@ -1,17 +1,10 @@
 ﻿<?php
 error_reporting(0);
 ini_set('display_errors', 1);
-
-// Начинаем сессию, если она еще не начата
 if (!isset($_SESSION)) {
     session_start();
 }
-
-// Уничтожаем все данные сессии
 $_SESSION = array();
-
-// Если требуется уничтожить сессию полностью, необходимо также удалить куки сессии.
-// Это гарантирует, что текущий идентификатор сессии будет недействителен.
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -19,14 +12,10 @@ if (ini_get("session.use_cookies")) {
         $params["secure"], $params["httponly"]
     );
 }
-
-// Уничтожаем сессию
 session_destroy();
 
-// Перенаправляем пользователя на страницу авторизации
 echo "<script>window.location.replace('../Avtorization/avtorization.php');</script>";
-exit; // Важно завершить выполнение скрипта после перенаправления
+exit; 
 
-// Добавляем закрытие соединения с базой данных
-$conn->close();  // Предполагается, что объект соединения называется $conn, и его метод close() используется для закрытия соединения
+$conn->close(); 
 ?>
