594ae578782a13956e7500063f3c43758e77b519
AntikKrasn <146510237+AntikKrasn@users.noreply.github.com>
2024-05-23 21:46:04 +0500

    Update script.js

--- Task/script.js
+++ Task/script.js
@@ -1,1 +1,10 @@
-﻿
+﻿function popup() {
+    const popupContainer = document.createElement("div");
+    popupContainer.innerHTML = <div id="popupContainer"> <h1>Новая заметка</h1><textarea id="note-text" placeholder="Введите вашу заметку..."></textarea><div id="btn-container"><button id="submitBtn" onclick="createNote(); closePopup()">Создать</button><button id="closeBtn" onclick="closePopup()">Закрыть</button></div></div>;
+    document.body.appendChild(popupContainer);
+}
+
+function closePopup() {
+    const popupContainer = document.getElementById("popupContainer");
+    if(popupContainer) {
+        popupContainer.remove();}}
