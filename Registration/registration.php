73651eaff35aa4beb04f7ea620c368675c7caca2
AntikKrasn <146510237+AntikKrasn@users.noreply.github.com>
2024-05-23 22:01:24 +0500

    Update registration.php

--- Registration/registration.php
+++ Registration/registration.php
@@ -1,5 +1,5 @@
 <?php
-ob_start(); // начинаем буферизацию вывода
+ob_start(); 
 
 require_once('db.php');
  
@@ -12,15 +12,11 @@ $email = $_POST['email'];
 
 $sql = "INSERT INTO `registr` (familia, name, number, login, pass, email) VALUES ('$familia', '$name', '$number', '$login', '$pass', '$email')";
 
-// Выполняем запрос к базе данных
 if ($conn->query($sql) === TRUE) {
-    // Если запрос выполнен успешно, выполняем перенаправление
     header("Location:..\Avtorization\avtorization.php");
-    exit(); // Важно вызвать exit() после перенаправления, чтобы прекратить дальнейшее выполнение скрипта
+    exit(); 
 } else {
-    // Если запрос не выполнен успешно, можно выполнить другие действия или вывести сообщение об ошибке
     echo "Ошибка: " . $conn->error;
 }
-
-ob_end_flush(); // отправляем буферизированный вывод на экран
+ob_end_flush(); 
 ?>
