e7ed335733973106f211df0f5b68b90981eedce9
AntikKrasn <146510237+AntikKrasn@users.noreply.github.com>
2024-05-23 21:50:17 +0500

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,19 +1,17 @@
 ﻿<?php
 error_reporting(0);
-session_start(); // начинаем сессию
-ob_start(); // начинаем буферизацию вывода
+session_start(); 
+ob_start(); 
 
-// Проверяем, установлены ли данные пользователя в сессии
 if(isset($_SESSION['login'], $_SESSION['name'], $_SESSION['number'], $_SESSION['email'])) {
-    // Если данные пользователя есть в сессии, присваиваем их переменным
     $login = $_SESSION['login'];
     $name = $_SESSION['name'];
     $number = $_SESSION['number'];
     $email = $_SESSION['email'];
     echo "<script>window.location.replace('../User/profile.php');</script>";
-    exit; // Выход из скрипта, чтобы предотвратить вывод HTML-кода ниже
+    exit; 
 } else {
     echo "<script>window.location.replace('Avtorization/avtorization.php');</script>";
-    exit; // Выход из скрипта
+    exit; 
 }
 ?>
